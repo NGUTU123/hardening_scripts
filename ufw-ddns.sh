1a461fad233fff8cc86aa41c2bd7add6ab02c717
NGUTU123 <ngutu@yopmail.com>
2022-04-05 15:57:50 +0700

    Update ufw-ddns.sh

--- ufw-ddns.sh
+++ ufw-ddns.sh
@@ -13,27 +13,7 @@ if [ "$new_ip" = "$old_ip" ] ; then
     echo IP address has not changed
 else
     if [ -n "$old_ip" ] ; then
-        /usr/sbin/ufw delete allow from $old_ip to any port 13689
-	/usr/sbin/ufw delete allow from $old_ip to any port 8083
-	/usr/sbin/ufw delete allow from $old_ip to any port 80
-	/usr/sbin/ufw delete allow from $old_ip to any port 443
-	/usr/sbin/ufw delete allow from $old_ip to any port 143
-	/usr/sbin/ufw delete allow from $old_ip to any port 993
-	/usr/sbin/ufw delete allow from $old_ip to any port 110
-	/usr/sbin/ufw delete allow from $old_ip to any port 995
-	/usr/sbin/ufw delete allow from $old_ip to any port 25
-	/usr/sbin/ufw delete allow from $old_ip to any port 465
-	/usr/sbin/ufw delete allow from $old_ip to any port 587
+    /usr/sbin/ufw delete allow from $old_ip to any port 13689
     fi
     /usr/sbin/ufw allow from $new_ip to any port 13689 comment $HOSTNAME
-    /usr/sbin/ufw allow from $new_ip to any port 8083 comment $HOSTNAME
-    /usr/sbin/ufw allow from $new_ip to any port 80 comment $HOSTNAME
-    /usr/sbin/ufw allow from $new_ip to any port 443 comment $HOSTNAME
-    /usr/sbin/ufw allow from $new_ip to any port 143 comment $HOSTNAME
-    /usr/sbin/ufw allow from $new_ip to any port 993 comment $HOSTNAME
-    /usr/sbin/ufw allow from $new_ip to any port 110 comment $HOSTNAME
-    /usr/sbin/ufw allow from $new_ip to any port 995 comment $HOSTNAME
-    /usr/sbin/ufw allow from $new_ip to any port 25 comment $HOSTNAME
-    /usr/sbin/ufw allow from $new_ip to any port 465 comment $HOSTNAME
-    /usr/sbin/ufw allow from $new_ip to any port 587 comment $HOSTNAME
 fi
