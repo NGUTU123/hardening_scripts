61f8cabf9e965732f3f97434a69f97353edb040a
NGUTU123 <ngutu@yopmail.com>
2022-04-07 08:45:15 +0700

    Update CBI.sh

--- CBI.sh
+++ CBI.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 python3 main.py -u https://vinabiz.us/categories/xaphuong/phuong-ve-an/3200320033003000310030003700 -s 1 -e 10 -o VeAn__BacNinh & &&
 python3 main.py -u https://vinabiz.us/categories/xaphuong/phuong-tien-an/3200320033003000310030003900 -s 1 -e 22 -o TienAn__BacNinh &
