9ce3d57fbf2af01d075b10fd373dce9653a255dc
NGUTU123 <ngutu@yopmail.com>
2022-04-08 20:46:11 +0700

    Update and rename ipset-update-dyn.sh to dns-ip-update.sh

--- ipset-update-dyn.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-iptables -N myDDNS 2>/dev/null;iptables -I INPUT -j myDDNS;iptables -F myDDNS && iptables -I myDDNS -p tcp -s $(dig +short recuc.ddns.net) -m state --state NEW -m tcp -j ACCEPT
