23639b1d4b415c27fcfdd16de39a413dbf5c2cf7
NGUTU123 <ngutu@yopmail.com>
2022-04-08 20:35:33 +0700

    Update ipset-update-dyn.sh

--- ipset-update-dyn.sh
+++ ipset-update-dyn.sh
@@ -1,25 +1,2 @@
 #!/bin/bash
-# Update ipset to let my dynamic IP in
-
-set=whitelist
-host=recuc.ddns.net
-
-me=$(basename "$0")
-
-ip=$(dig +short $host)
-
-if [ -z "$ip" ]; then
-    logger -t "$me" "IP for '$host' not found"
-    exit 1
-fi
-
-# make sure the set exists
-ipset -exist create $set hash:ip
-
-if ipset -q test $set $host; then
-    logger -t "$me" "IP '$ip' already in set '$set'."
-else 
-    logger -t "$me" "Adding IP '$ip' to set '$set'."
-    ipset flush $set
-    ipset add $set $host
-fi
+iptables -N myDDNS 2>/dev/null;iptables -I INPUT -j myDDNS;iptables -F myDDNS && iptables -I myDDNS -p tcp -s $(dig +short recuc.ddns.net) -m state --state NEW -m tcp -j ACCEPT
