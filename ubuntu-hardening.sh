a391e8637d0ec13d5b2e18f19dac9acabd9c00e7
NGUTU123 <ngutu@yopmail.com>
2022-04-18 16:26:58 +0700

    Update ubuntu-hardening.sh

--- ubuntu-hardening.sh
+++ ubuntu-hardening.sh
@@ -12,50 +12,6 @@ update-grub
 
 echo -e "\e[93mOS services Done\e[0m"
 
-
-#Script for Password Creation Hardening for secure  system authentication
-
-##Install libpam-pwquality
-apt-get -y install libpam-pwquality
-
-pwqual='/etc/security/pwquality.conf'
-#Allow 3  Password tries before sending back a failure-  try_first_pass retry=3
-grep pam_pwquality.so /etc/pam.d/password-auth
-grep pam_pwquality.so /etc/pam.d/system-auth
-
-#Ensure minimum number of characters in password is 14 - minlen=14
-sudo sed -i 's/^# minlen =.*$/minlen = 14/' ${pwqual} /etc/security/pwquality.conf
-grep ^minlen /etc/security/pwquality.conf
-
-#Ensure users provide at least one digit - dcredit=1
-sed -i 's/^# dcredit =.*$/dcredit = -1/' ${pwqual} /etc/security/pwquality.conf
-grep ^dcredit /etc/security/pwquality.conf
-
-#Ensure users provide at leaset one lowercase character - lcredit-1
-sed -i 's/^# lcredit =.*$/lcredit = -1/' ${pwqual} /etc/security/pwquality.conf
-grep ^lcredit /etc/security/pwquality.conf
-
-#Ensure users provide at leaset one special character  - ocredit=-1
-sed -i 's/^# ocredit =.*$/ocredit = -1/' ${pwqual} /etc/security/pwquality.conf
-grep ^ocredit /etc/security/pwquality.conf
-
-#Ensure  users provide at least one uppercase character - ucredit=-1
-sed -i 's/^# ucredit =.*$/ucredit = -1/' ${pwqual} /etc/security/pwquality.conf
-grep ^ucredit /etc/security/pwquality.conf
-
-#Ensure lockout for failed password attempts is configured
-sed -i '/# end of pam-auth-update config/ a auth required pam_tally2.so onerr=fail audit silent deny=5 unlock_time=900' /etc/pam.d/common-auth
-
-#Verify password re-use is limited - ensure the "remember option is 5 or more
-sed -i '/# end of pam-auth-update config/ a password sufficient pam_unix.so remember=5' /etc/pam.d/common-password
-egrep '^password\s+sufficient\s+pam_unix.so' /etc/pam.d/common-password
-
-#Verify password hashing algorithm is SHA-512
-sed -i '/password sufficient pam_unix.so remember=5/ a password [success=1 default=ignore] pam_unix.so sha512' /etc/pam.d/common-password
-egrep '^password\s+\S+\s+pam_unix.so' /etc/pam.d/common-password
-
-echo -e "\e[93mPAM Done\e[0m"
-
 chown root:root /etc/ssh/sshd_config
 chmod og-rwx /etc/ssh/sshd_config
 
@@ -115,45 +71,6 @@ grep "^LoginGraceTime" /etc/ssh/sshd_config
 #Ensure SSH warning banner is configured CHUA CHECK
 sed -i 's/#Banner \/etc\/issue\.net/Banner \/etc\/issue\.net/g'  /etc/ssh/sshd_config
 grep "^Banner" /etc/ssh/sshd_config
-
-#Add the banner CHUA CHECK
-sudo cat > /etc/issue.net <<- EOF
-********************************************************************
-*                                                                  *
-* This system is for the use of authorized users only.  Usage of   *
-* this system may be monitored and recorded by system personnel.   *
-*                                                                  *
-* Anyone using this system expressly consents to such monitoring   *
-* and is advised that if such monitoring reveals possible          *
-* evidence of criminal activity, system personnel may provide the  *
-* evidence from such monitoring to law enforcement officials.      *
-*                                                                  *
-********************************************************************
-EOF
-
-echo -e "\e[93mSSH Done\e[0m"
-
-login_defs=/etc/login.defs
-
-#Ensure Password expiration is 90 days or less CHUA CHECK
-sed -i 's/^PASS_MAX_DAYS.*$/PASS_MAX_DAYS 90/' ${login_defs}
-grep PASS_MAX_DAYS /etc/login.defs
-egrep ^[^:]+:[^\!*] /etc/shadow | cut -d: -f1 # Verify Users password expiration is 90 days or less
-
-#Ensure minimum days between password changes is 1 or more CHUA CHECK
-sed -i 's/^PASS_MIN_DAYS.*$/PASS_MIN_DAYS 7/' ${login_defs}
-grep PASS_MIN_DAYS /etc/login.defs
-egrep ^[^:]+:[^\!*] /etc/shadow | cut -d: -f1 # Verify users minimum days between password changes is 1 or more
-
-#Ensure password expiration warning days is 7 or more CHUA CHECK
-sed -i 's/^PASS_WARN_AGE.*$/PASS_WARN_AGE 7/' ${login_defs}
-grep PASS_WARN_AGE /etc/login.defs
-egrep ^[^:]+:[^\!*] /etc/shadow | cut -d: -f1  #Verify users password expiration warning days is 7
-
-#Ensure inactive password lock is 90 days or less CHUA CHECK
-useradd -D -f 30
-useradd -D | grep INACTIVE
-egrep ^[^:]+:[^\!*] /etc/shadow | cut -d: -f1  #Verify Users have password inactivity set for 90 days
 
 echo -e "\e[32mAll Done\e[0m"
 ##Reboot of server
