7f5f10480c954c4ee6e9b9f263636785bf999de6
NGUTU123 <ngutu@yopmail.com>
2022-04-18 10:55:00 +0700

    Update ubuntu-hardening.sh

--- ubuntu-hardening.sh
+++ ubuntu-hardening.sh
@@ -284,39 +284,6 @@ echo -e "\e[93mUser Account Done\e[0m"
 #Ensure password fields are not empty CHUA CHECK
 cat /etc/shadow | awk -F: '($2 == "" ) { print $1 " does not have a password "}'
 
-#Ensure root PATH Integrity CHUA CHECK
-if [ "`echo $PATH | grep :: `" != "" ]; then
- echo "Empty Directory in PATH (::)"
-fi
-if [ "`echo $PATH | grep :$`" != "" ]; then
- echo "Trailing : in PATH"
-fi
-p=`echo $PATH | sed -e 's/::/:/' -e 's/:$//' -e 's/:/ /g'`
-set -- $p
-while [ "$1" != "" ]; do
- if [ "$1" = "." ]; then
- echo "PATH contains ."
- shift
- continue
- fi
- if [ -d $1 ]; then
- dirperm=`ls -ldH $1 | cut -f1 -d" "`
- if [ `echo $dirperm | cut -c6 ` != "-" ]; then
- echo "Group Write permission set on directory $1"
- fi
- if [ `echo $dirperm | cut -c9 ` != "-" ]; then
- echo "Other Write permission set on directory $1"
- fi
- dirown=`ls -ldH $1 | awk '{print $3}'`
- if [ "$dirown" != "root" ] ; then
- echo $1 is not owned by root
- fi
- else
- echo $1 is not a directory
-  fi
- shift
-done
-
 #Ensure no users have .forward files CHUA CHECK
 for dir in `cat /etc/passwd |\
 awk -F: '{ print $6 }'`; do
